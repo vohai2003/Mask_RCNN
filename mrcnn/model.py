e0bba9d1c4420bb8d969e13d8f3a349c22d82063
Arun Kumar Perumal <39132298+arunkumarperumal@users.noreply.github.com>
2024-08-25 11:39:47 +0530

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2336,7 +2336,7 @@ class MaskRCNN(object):
             callbacks=callbacks,
             validation_data=val_generator,
             validation_steps=self.config.VALIDATION_STEPS,
-            max_queue_size=100,
+            #max_queue_size=100,
             workers=workers,
             use_multiprocessing=workers > 1,
         )
