ac142686d97cfca6f7e8c573d4144a78e441700e
Arun Kumar Perumal <39132298+arunkumarperumal@users.noreply.github.com>
2024-08-25 11:02:01 +0530

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2267,7 +2267,7 @@ class MaskRCNN(object):
             self.config.NAME.lower(), now))
 
         # Path to save after each epoch. Include placeholders that get filled by Keras.
-        self.checkpoint_path = os.path.join(self.log_dir, "mask_rcnn_{}_*epoch*.h5".format(
+        self.checkpoint_path = os.path.join(self.log_dir, "mask_rcnn_{}_*epoch*.weights.h5".format(
             self.config.NAME.lower()))
         self.checkpoint_path = self.checkpoint_path.replace(
             "*epoch*", "{epoch:04d}")
