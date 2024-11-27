c4692d65761e685db6c82d59cc132fb797884ea6
vohai2003 <63694226+vohai2003@users.noreply.github.com>
2024-11-27 15:39:46 +0700

    Update utils.py

--- mrcnn/utils.py
+++ mrcnn/utils.py
@@ -530,7 +530,7 @@ def minimize_mask(bbox, mask, mini_shape):
             raise Exception("Invalid bounding box with area of zero")
         # Resize with bilinear interpolation
         m = resize(m, mini_shape)
-        mini_mask[:, :, i] = np.around(m).astype(np.bool_)
+        mini_mask[:, :, i] = np.around(m).astype(bool)
     return mini_mask
 
 
